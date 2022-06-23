ce5c9b05666848db32c31e5848f342c62da591a2
sakrapeepais <paisalnan_s@silpakorn.edu>
2022-06-23 14:04:38 +0700

    update the testcases and the quiz

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -2,8 +2,12 @@ package com.example.project;
 
 public class Hello {
 
+	public static boolean floatingPointComparison(double x, double y){
+		return false;
+	}
+
 	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
+
 	}
 
 }
--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -9,8 +9,11 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.*;
 
-public class TestHello {
+// Write a Java program that reads in two floating-point numbers and tests whether they are the same up to three decimal
+// places.
 
+public class TestHello {
+    /*
    @Test
    public void testHelloWorld()
    {
@@ -27,4 +30,46 @@ public class TestHello {
      // undo the binding in System
      System.setOut(originalOut);
    }
+    */
+
+    @Test
+    public void testFloatingPointOne()
+    {
+        double x = 250.2578;
+        double y = 258.2579;
+
+        boolean chkFlag = Hello.floatingPointComparison(x, y);
+        assertEquals(chkFlag, false);
+    }
+
+    @Test
+    public void testFloatingPointTwo()
+    {
+        double x = -105.7509;
+        double y = 230.7508;
+
+        boolean chkFlag = Hello.floatingPointComparison(x, y);
+        assertEquals(chkFlag, false);
+    }
+
+    @Test
+    public void testFloatingPointThree()
+    {
+        double x = 99.230;
+        double y = 99.2301;
+
+        boolean chkFlag = Hello.floatingPointComparison(x, y);
+        assertEquals(chkFlag, true);
+    }
+
+    @Test
+    public void testFloatingPointFour()
+    {
+        double x = 99.23;
+        double y = 99.2301;
+
+        boolean chkFlag = Hello.floatingPointComparison(x, y);
+        assertEquals(chkFlag, true);
+    }
+
 }
